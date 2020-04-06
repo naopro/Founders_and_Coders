99817522338e7e14b6b38200153f68ec1fd12e03
Naomi <naopro@users.noreply.github.com>
2020-04-06 18:50:42 +0100

    html: add class to carousel js: failed eventListener attempt

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var changeImage = document.getElementById("carousel");
-changeImage.addEventListener("click", function(){ 
-    
+changeImage.addEventListener("click", function(){document.getElementsByClassName('carousel-img').src = './images/carousel images/nets'
+
 }
