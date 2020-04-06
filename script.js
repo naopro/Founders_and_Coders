0dd915cb81020101c15d71f7dc525d3c5536a13d
Naomi <naopro@users.noreply.github.com>
2020-04-06 22:24:35 +0100

    .js: Entering imgs into an array

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
     // var rightButton = document.getElementById("right-button");
     var carouselImages = document.getElementById("carousel-img");
 
-    leftButton.addEventListener('click', function () { carouselImages.src = './images/carousel_images/govball.jpg' });
+    leftButton.addEventListener('click', function () { carouselImages.src = imgArray[0] });
 });
+
+var imgArray = [
+    "./images/carousel_images/govball.jpg",
+    "./images/carousel_images/empirestate.jpg",
+    "./images/carousel_images/nets.jpg",
+    "./images/carousel_images/vessel.jpg"
+]
