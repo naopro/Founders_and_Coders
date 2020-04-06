6bfddef31f7663d997d13721c3d3e8224f891a1c
Naomi <naopro@users.noreply.github.com>
2020-04-06 20:05:17 +0100

    Tweaks Renaming of folder containing carousel imgs Js: trying to understand the structure of eventListeners

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-var changeImage = document.getElementById("carousel");
-changeImage.addEventListener("click", function(){document.getElementsByClassName('carousel-img').src = './images/carousel images/nets'
+// var carousel = document.getElementsByClassName("desc-4");
 
-}
+// carousel.addEventListener("click", function () { carousel.style.background = "green" });
+
+// // function changeImage(){}
+
+// // changeImage.addEventListener("click", function () {
+// //     document.getElementsByClassName('carousel-img').src = './images/carousel images/nets', true
+
+// })
+
+document.addEventListener('DOMContentLoaded', function () {
+    var leftButton = document.getElementById("left-button");
+    // var rightButton = document.getElementById("right-button");
+    var carouselImages = document.getElementById("carousel-img");
+
+    leftButton.addEventListener('click', function () { carouselImages.src = './images/carousel_images/govball.jpg' });
+});
