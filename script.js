4d2589eb3e1fa8455b3a327438c8dc425546d54a
Naomi <naopro@users.noreply.github.com>
2020-04-06 21:11:13 +0100

    Successful eventListener - to develop Changes from one image to another Needs to incorporate other images

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// var carousel = document.getElementsByClassName("desc-4");
-
-// carousel.addEventListener("click", function () { carousel.style.background = "green" });
-
-// // function changeImage(){}
-
-// // changeImage.addEventListener("click", function () {
-// //     document.getElementsByClassName('carousel-img').src = './images/carousel images/nets', true
-
-// })
-
 document.addEventListener('DOMContentLoaded', function () {
     var leftButton = document.getElementById("left-button");
     // var rightButton = document.getElementById("right-button");
