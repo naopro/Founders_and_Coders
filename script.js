def1ab9de3ce6ba616a61445b73584cb8ce65d08
Naomi <naopro@users.noreply.github.com>
2020-04-06 23:19:20 +0100

    Add eventListener to rightButton

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     var leftButton = document.getElementById("left-button");
-    // var rightButton = document.getElementById("right-button");
+    var rightButton = document.getElementById("right-button");
     var carouselImages = document.getElementById("carousel-img");
 
     leftButton.addEventListener('click', function () { carouselImages.src = imgArray[0] });
+
+    var imgArray = [
+        "./images/carousel_images/govball.jpg",
+        "./images/carousel_images/empirestate.jpg",
+        "./images/carousel_images/nets.jpg",
+        "./images/carousel_images/vessel.jpg"
+    ]
+
+
+    rightButton.addEventListener('click', function () { carouselImages.src = imgArray[3]; });
 });
 
 var imgArray = [
