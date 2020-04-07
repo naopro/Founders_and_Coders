4de1038829fc9cb9ec102bf7868a3e25b6e7321c
Naomi <naopro@users.noreply.github.com>
2020-04-07 15:55:50 +0100

    Successful iterate through array for leftButton!! DOMContentLoaded was added for functionality Reverse needs to be added for rightButton

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-// document.addEventListener('DOMContentLoaded', function () {
-var leftButton = document.getElementById("left-button");
-var rightButton = document.getElementById("right-button");
-var carouselImages = document.getElementById("carousel-img");
+document.addEventListener('DOMContentLoaded', function () {
+    var leftButton = document.getElementById("left-button");
+    // var rightButton = document.getElementById("right-button");
+    var carouselImages = document.getElementById("carousel-img");
 
+    var imgArray = [
+        "./images/carousel_images/empirestate.jpg",
+        "./images/carousel_images/govball.jpg",
+        "./images/carousel_images/nets.jpg",
+        "./images/carousel_images/vessel.jpg"
+    ];
+    var imgIndex = 1;
 
-
-
-var imgArray = [
-    "./images/carousel_images/govball.jpg",
-    "./images/carousel_images/empirestate.jpg",
-    "./images/carousel_images/nets.jpg",
-    "./images/carousel_images/vessel.jpg"
-];
-
-
-// rightButton.addEventListener('click', function () { carouselImages.src = imgArray[3]; });
-// leftButton.addEventListener('click', changepic());
-// rightButton.addEventListener('click', changepic());
-
-leftButton.addEventListener('click', function (carouselImages) {
-    for (var i = 0; i < imgArray.length; i++) {
-        carouselImages.src(imgArray[i])
-    }
+    leftButton.addEventListener('click', function () {
+        carouselImages.setAttribute("src", imgArray[imgIndex]);
+        imgIndex++;
+        if (imgIndex > 3) { imgIndex = 0; };
+    });
 });
