ec773d094feba0b9f9fd404cdd1cd58e25509062
Naomi <naopro@users.noreply.github.com>
2020-04-07 16:37:38 +0100

    Successfully iterates up and down array!

--- script.js
+++ script.js
@@ -9,17 +9,24 @@ document.addEventListener('DOMContentLoaded', function () {
         "./images/carousel_images/nets.jpg",
         "./images/carousel_images/vessel.jpg"
     ];
+
+    // THE POSITION THAT THE FIRST CLICK GOES TO: //
     var imgIndex = 1;
 
+    // RIGHT BUTTON ORDER: SRC, 1, 2, 3, 0 // AS DESIRED
     rightButton.addEventListener('click', function () {
         carouselImages.setAttribute("src", imgArray[imgIndex]);
         imgIndex++;
         if (imgIndex > 3) { imgIndex = 0; };
     });
 
-    // rightButton.addEventListener('click', function () {
-    //     carouselImages.setAttribute("src", imgArray[imgIndex]);
-    //     imgIndex++;
-    //     if (imgIndex > 3) { imgIndex = 0; };
-    // });
+    // THE POSITION THAT THE FIRST CLICK GOES TO: //
+    var imgGloss = 3
+
+    // LEFT BUTTON ORDER: SRC, 3, 2, 1, 0
+    leftButton.addEventListener('click', function () {
+        carouselImages.setAttribute("src", imgArray[imgGloss]);
+        imgGloss--
+        if (imgGloss < 0) { imgGloss = 3; };
+    });
 });
