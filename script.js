61ac4d51fab9e51743a76786a440dc1b99cec206
Naomi <naopro@users.noreply.github.com>
2020-04-07 15:21:54 +0100

    Attempting to loop thorugh imgArray with for loop

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var leftButton = document.getElementById("left-button");
-    var rightButton = document.getElementById("right-button");
-    var carouselImages = document.getElementById("carousel-img");
-
-    leftButton.addEventListener('click', function () { carouselImages.src = imgArray[0] });
-
-    var imgArray = [];
-    imgArray[0] = "./images/carousel_images/govball.jpg",
-        imgArray[1] = "./images/carousel_images/empirestate.jpg",
-        imgArray[2] = "./images/carousel_images/nets.jpg",
-        imgArray[3] = "./images/carousel_images/vessel.jpg"
+// document.addEventListener('DOMContentLoaded', function () {
+var leftButton = document.getElementById("left-button");
+var rightButton = document.getElementById("right-button");
+var carouselImages = document.getElementById("carousel-img");
 
 
 
-    var i = 0;
-    function changepic(3);
-    if (i < imgArray.length; i++)
-carouselImages = (imgArray[i++]);
+
+var imgArray = [
+    "./images/carousel_images/govball.jpg",
+    "./images/carousel_images/empirestate.jpg",
+    "./images/carousel_images/nets.jpg",
+    "./images/carousel_images/vessel.jpg"
+];
+
+
 // rightButton.addEventListener('click', function () { carouselImages.src = imgArray[3]; });
-rightButton.addEventListener('click', changepic());
-});
+// leftButton.addEventListener('click', changepic());
+// rightButton.addEventListener('click', changepic());
 
-// var imgArray = [
-//     "./images/carousel_images/govball.jpg",
-//     "./images/carousel_images/empirestate.jpg",
-//     "./images/carousel_images/nets.jpg",
-//     "./images/carousel_images/vessel.jpg"
-// ]
+leftButton.addEventListener('click', function (carouselImages) {
+    for (var i = 0; i < imgArray.length; i++) {
+        carouselImages.src(imgArray[i])
+    }
+});
