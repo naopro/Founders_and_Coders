95580a268f6015beece294851d208eb768fab357
Naomi <naopro@users.noreply.github.com>
2020-04-07 01:34:53 +0100

    Trial and error wtih .js Trying to create a loop to iterate through imgs - needs improvement

--- script.js
+++ script.js
@@ -5,20 +5,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
     leftButton.addEventListener('click', function () { carouselImages.src = imgArray[0] });
 
-    var imgArray = [
-        "./images/carousel_images/govball.jpg",
-        "./images/carousel_images/empirestate.jpg",
-        "./images/carousel_images/nets.jpg",
-        "./images/carousel_images/vessel.jpg"
-    ]
+    var imgArray = [];
+    imgArray[0] = "./images/carousel_images/govball.jpg",
+        imgArray[1] = "./images/carousel_images/empirestate.jpg",
+        imgArray[2] = "./images/carousel_images/nets.jpg",
+        imgArray[3] = "./images/carousel_images/vessel.jpg"
 
 
-    rightButton.addEventListener('click', function () { carouselImages.src = imgArray[3]; });
+
+    var i = 0;
+    function changepic(3);
+    if (i < imgArray.length; i++)
+carouselImages = (imgArray[i++]);
+// rightButton.addEventListener('click', function () { carouselImages.src = imgArray[3]; });
+rightButton.addEventListener('click', changepic());
 });
 
-var imgArray = [
-    "./images/carousel_images/govball.jpg",
-    "./images/carousel_images/empirestate.jpg",
-    "./images/carousel_images/nets.jpg",
-    "./images/carousel_images/vessel.jpg"
-]
+// var imgArray = [
+//     "./images/carousel_images/govball.jpg",
+//     "./images/carousel_images/empirestate.jpg",
+//     "./images/carousel_images/nets.jpg",
+//     "./images/carousel_images/vessel.jpg"
+// ]
